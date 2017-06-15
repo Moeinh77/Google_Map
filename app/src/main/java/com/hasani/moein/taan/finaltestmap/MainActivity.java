d9ee0b9ffde2cf1a20c5fd3912db17596de80703
Moeinh77 <moeinhasani77@gmail.com>
2017-06-15 13:36:11 +0430

    Initial commit

---
+++ app/src/main/java/com/hasani/moein/taan/finaltestmap/MainActivity.java
@@ -0,0 +1,45 @@
+package com.hasani.moein.taan.finaltestmap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mbutton;
+    private EditText xcor;
+    private EditText ycor;
+
+    private String x;
+    private String y;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        xcor=(EditText)findViewById(R.id.xcordinate);
+        ycor=(EditText)findViewById(R.id.ycordinate);
+
+        x=xcor.getText().toString();
+        y=ycor.getText().toString();
+
+
+        mbutton=(Button)findViewById(R.id.map);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                int a = Integer.parseInt(xcor.getText().toString());
+                int b = Integer.parseInt(ycor.getText().toString());
+                intent.putExtra("x",a);
+                intent.putExtra("y",b);
+
+                startActivity(intent);
+            }
+        });
+    }
+}

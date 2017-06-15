de538f0a0fe873f551e7c6001c8f3be8b40065f0
Moeinh77 <moeinhasani77@gmail.com>
2017-06-15 15:36:26 +0430

    v3

--- app/src/main/java/com/hasani/moein/taan/finaltestmap/MapsActivity.java
+++ app/src/main/java/com/hasani/moein/taan/finaltestmap/MapsActivity.java
@@ -1,10 +1,15 @@
 package com.hasani.moein.taan.finaltestmap;
 
 import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,71 +17,92 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener {
+
+//    protected synchronized void buildGoogleApiClient() {
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API)
+//                .build();
+//        mGoogleApiClient.connect();
+//    }
 
     private GoogleMap mMap;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    //////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onLocationChanged(Location location) {
 
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //buildGoogleApiClient();
+        try{
+            mMap.setMyLocationEnabled(true);
+            Location myLocation = googleMap.getMyLocation();
 
-        if(mMap==null){
-            Toast.makeText(getApplicationContext(),"Connection failed",Toast.LENGTH_SHORT).show();
+                Bundle recevier=getIntent().getExtras();
+                float x=recevier.getFloat("x");
+                float y=recevier.getFloat("y");
+
+                LatLng mycordinate = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(mycordinate).title("Marker"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(mycordinate));
+
+        } catch(SecurityException e){
+
+            Toast.makeText(getApplicationContext(),"App doesn't have the permission to gps... ",Toast.LENGTH_SHORT).show();
         }
-        else{
-        Bundle recevier=getIntent().getExtras();
-            float x=recevier.getFloat("x");
-            float y=recevier.getFloat("y");
-
-        LatLng mycordinate = new LatLng(x, y);
-        mMap.addMarker(new MarkerOptions().position(mycordinate).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(mycordinate));}
-    }
 
 
-//
-//    private void setUpMapIfNeeded() {
-//        // Do a null check to confirm that we have not already instantiated the map.
-//        if (mMap == null) {
-//            // Try to obtain the map from the SupportMapFragment.
-//            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-//                    .getMap();
-//            mMap.setMyLocationEnabled(true);
-//            // Check if we were successful in obtaining the map.
-//            if (mMap != null) {
-//
-//
-//                mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-//
-//                    @Override
-//                    public void onMyLocationChange(Location arg0) {
-//                        // TODO Auto-generated method stub
-//
-//                        mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
-//                    }
-//                });
-//
-//            }
-//        }
-//    }
-}
+
+
+
+}}
